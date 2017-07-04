4f2be0af00e25dce2fb16bb9bcd2f5b37aa14e41
Thdegraaff <t.de.graaff@vu.nl>
2017-07-04 15:47:08 +0200

    Reading data now works for first_stage

--- R/first_stage.R
+++ R/first_stage.R
@@ -1,10 +1,10 @@
 library("dplyr")
-library("maxLik")
 
 first_stage <- function(id, X, Z){
   X <- data.frame(id,X)
   Z <- data.frame(id,Z)
   datacity <- Z %>%
     group_by(id) %>%
-    print(summarise_all(funs(mean)))
+    summarise_all(funs(mean))
+  print(datacity)
 }
