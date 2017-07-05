7808fcc8230a612a9863dd96baeac579daaa1d7c
Thomas de Graaff <t.de.graaff@vu.nl>
2017-07-05 14:47:49 +0200

    First draft of complete first-stage

--- R/first_stage.R
+++ R/first_stage.R
@@ -1,10 +1,55 @@
 library("dplyr")
+library("maxLik")
 
-first_stage <- function(id, X, Z){
-  X <- data.frame(id,X)
-  Z <- data.frame(id,Z)
-  datacity <- Z %>%
-    group_by(id) %>%
+first_stage <- function(code, Z, X){
+
+  Z <- data.frame(code,Z)
+  X <- data.frame(code,X)
+
+  # First, create the city specific data matrices
+  datacity <- X %>%
+    group_by(code) %>%
     summarise_all(funs(mean))
-  print(datacity)
+  datacity <- data.matrix(datacity)
+  rownames(datacity) <- datacity[,1]
+
+  # Second, create the individual specific data matrices (select here your variables)
+  data <- data.matrix(Z)
+  id <- as.numeric(as.factor(data[,1]))
+
+  # scaling the data
+  data[,2:ncol(data)]<-scale(data[,2:ncol(data)])
+  datacity <- scale(datacity)
+
+  # make sure each variable has a name
+  varNames <- c(outer(colnames(data[,2:ncol(data)]), colnames(datacity[,2:ncol(datacity)]), FUN= paste, sep=":"))
+  # There should be one base catetory! Here the first
+  varNames <- c(rownames(datacity)[2:nrow(datacity)],varNames)
+
+  startValues = rep(0,length(varNames))
+  gradi <- matrix(rep(0, nrow(data)*length(varNames)), nrow(data), length(varNames))
+  names(startValues) <- varNames
+
+  LogLikFun <- function(param) {
+    Prob <- rep(0, nrow(data))
+    mat <- diag(nrow(datacity))
+    mat <- mat[,-1]
+    for (i in 1:nrow(data)){
+      xij <- cbind(mat,kronecker(t(data[i,2:ncol(data)]),(datacity[,2:ncol(datacity)]), make.dimnames=TRUE))
+      xbij <- xij[,nrow(datacity):length(varNames)] %*% param[nrow(datacity):length(varNames)] + c(0,param[1:nrow(datacity)-1])
+      pij <- exp(xbij)/sum(exp(xbij))
+      Prob[i] <- log(pij[id[i]])
+      Yvec <- rep(0,nrow(datacity))
+      Yvec[id[i]] <- 1
+      Yvec <- t(Yvec - pij)%*%xij
+      gradi[i,] <<- Yvec[,]
+    }
+    Prob
+  }
+
+  gradlik <- function(param) gradi
+
+  Estimates <- maxLik(LogLikFun, gradlik, start=startValues, print.level=3, method = "BHHH")
+
+  return(Estimates)
 }
