54338d801d91e85207b855f5a8f7096bbdeca2e2
orlevko <orlevko@gmail.com>
2017-07-17 11:23:18 +0200

    updated vignette

--- vignettes/my-vignette.R
+++ vignettes/my-vignette.R
@@ -1,24 +1,35 @@
 ## ------------------------------------------------------------------------
 library(sortingmod)
 
-## ------------------------------------------------------------------------
-load("~/sortingmod/data/municipality.rda")
-
-## ------------------------------------------------------------------------
-head(municipality)
+## ---- echo = FALSE-------------------------------------------------------
+load("~/sortingmod/data/municipality_new.rda")
+code_name <- c("mun_code")
+Z_names <- c("age","income","hh_kids")
+X_names <- c("lnprice","nature","monuments")
+head(municipality[,c("id",Z_names,code_name,X_names)])
+municipality <- subset(municipality, randstad ==1)
 
 ## ---- results = "hide"---------------------------------------------------
 code_name <- c("mun_code")
 Z_names <- c("age","income")
 X_names <- c("lnprice","nature","monuments")
-s1.results <- first_stage(code_name, Z_names, X_names, dat = municipality)
+# s1.results <- first_stage(code_name, Z_names, X_names, data = municipality)
 
-## ------------------------------------------------------------------------
-summary(s1.results)
+## ---- echo = FALSE-------------------------------------------------------
+# summary(s1.results)
 
-## ---- results = "hide"---------------------------------------------------
-# endog <- c("lnprice")
-# inst.var <- sorting_inst(s1.results, endog, dat = municipality) ### Returns an error when build & reload
+## ---- eval = FALSE-------------------------------------------------------
+#  second_stage(s1.results, data)
+#  
+#  endog <- ("lnprice")
+#  second_stage(s1.results, data, endog = endog, instr = instrument)
+
+## ---- eval = FALSE-------------------------------------------------------
+#  endog <- ("lnprice")
+#  phat <- sorting_inst(s1.results, endog, data, n.iterations = 3, stepsize = 0.05)
+
+## ---- echo = FALSE-------------------------------------------------------
+# summary(phat$IV_results)
 
 ## ---- fig.show='hold'----------------------------------------------------
 plot(1:10)
