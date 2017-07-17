4e9bdd5c164f394a6c0612efb4e524a68c078554
orlevko <orlevko@gmail.com>
2017-07-17 16:14:01 +0200

    auto update

--- vignettes/my-vignette.R
+++ vignettes/my-vignette.R
@@ -1,30 +1,3 @@
 ## ------------------------------------------------------------------------
 library(sortingmod)
 
-## ---- echo = FALSE-------------------------------------------------------
-load("~/sortingmod/data/municipality_new.rda")
-code_name <- c("mun_code")
-Z_names <- c("age","income","hh_kids")
-X_names <- c("lnprice","nature","monuments")
-head(municipality[,c("id",Z_names,code_name,X_names)])
-municipality <- subset(municipality, randstad ==1)
-
-## ---- results = "hide"---------------------------------------------------
-code_name = c("mun_code")
-X_names = c("lnprice","kindergardens_1km","p_mig_west","nature","monuments","cafes_1km")
-Z_names = c("income","double_earner_hh","hh_kids","age", "migskill")
-s1.results <- first_stage(code_name, Z_names, X_names, data = municipality)
-
-## ---- eval = FALSE-------------------------------------------------------
-#  second_stage(s1.results, data)
-#  
-#  endog <- ("lnprice")
-#  second_stage(s1.results, data = municipality, endog = endog, instr = instrument)
-
-## ---- results = "hide"---------------------------------------------------
-endog <- ("lnprice")
-phat <- sorting_inst(s1.results, endog, data = municipality, stepsize = 0.02)
-
-## ---- echo = FALSE-------------------------------------------------------
-summary(phat$IV_results)
-
