4e89a9526117bc2fa29085ba5c99a065938a2325
orlevko <orlevko@gmail.com>
2017-07-17 11:49:06 +0200

    auto update

--- vignettes/my-vignette.R
+++ vignettes/my-vignette.R
@@ -10,31 +10,21 @@ head(municipality[,c("id",Z_names,code_name,X_names)])
 municipality <- subset(municipality, randstad ==1)
 
 ## ---- results = "hide"---------------------------------------------------
-code_name <- c("mun_code")
-Z_names <- c("age","income")
-X_names <- c("lnprice","nature","monuments")
-# s1.results <- first_stage(code_name, Z_names, X_names, data = municipality)
-
-## ---- echo = FALSE-------------------------------------------------------
-# summary(s1.results)
+code_name = c("mun_code")
+X_names = c("lnprice","kindergardens_1km","p_mig_west","nature","monuments","cafes_1km")
+Z_names = c("income","double_earner_hh","hh_kids","age", "migskill")
+s1.results <- first_stage(code_name, Z_names, X_names, data = municipality)
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  second_stage(s1.results, data)
 #  
 #  endog <- ("lnprice")
-#  second_stage(s1.results, data, endog = endog, instr = instrument)
+#  second_stage(s1.results, data = municipality, endog = endog, instr = instrument)
 
-## ---- eval = FALSE-------------------------------------------------------
-#  endog <- ("lnprice")
-#  phat <- sorting_inst(s1.results, endog, data, n.iterations = 3, stepsize = 0.05)
+## ---- results = "hide"---------------------------------------------------
+endog <- ("lnprice")
+phat <- sorting_inst(s1.results, endog, data = municipality, stepsize = 0.02)
 
 ## ---- echo = FALSE-------------------------------------------------------
-# summary(phat$IV_results)
-
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
-
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
+summary(phat$IV_results)
 
